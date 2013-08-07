865764bddff9149f9371b9e9db03208c1eddb941
Xiaocong Jin <jxcking@gmail.com>
2013-08-06 23:11:36 -0700

    display the content in index.html

--- web2.js
+++ web2.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send((fs.readFileSync('index.html')).toString());
+  response.send('hello world');
 });
 
 var port = process.env.PORT || 5000;
